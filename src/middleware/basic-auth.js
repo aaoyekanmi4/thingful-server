071e141b59f407432be86b0eb42b2b7c97e985eb
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-02-01 14:45:39 -0600

    switch basic auth with jwt tokens

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,5 +1,5 @@
 const AuthService = require('../auth/auth-service')
-const bcrypt = require('bcryptjs');
+
 
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || ''
@@ -28,7 +28,8 @@ function requireAuth(req, res, next) {
 
         return res.status(401).json({ error: 'Unauthorized request' })
       }
-            return bcrypt.compare(tokenPassword, user.password)
+      return AuthService.comparePasswords(tokenPassword, user.password)
+
              .then(passwordsMatch => {
                 if (!passwordsMatch) {
                   return res.status(401).json({ error: 'Unauthorized request' })
